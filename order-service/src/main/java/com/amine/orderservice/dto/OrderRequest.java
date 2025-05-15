481e9b1fd9be0eff11e6d71cda61fe6a13d6dd10
Amine Bouzidi <amineboozidi@gmail.com>
2025-05-15 13:19:07 +0100

    Initial commit

---
+++ order-service/src/main/java/com/amine/orderservice/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.amine.orderservice.dto;
+
+import com.amine.orderservice.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
